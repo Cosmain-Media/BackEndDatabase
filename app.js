096268329a1abd8058e71c74f19bdadfe26575c7
Preston <yaumanyoung@yahoo.com>
2020-10-15 10:52:56 -0700

    Added error handling for server

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cron = require('node-cron');
+require('dotenv').config();
+const {fetchVideos, updateCosmainVideos, getVideos, deleteVideos} = require('./controllers/videos');
+
+// ROUTES
+const videoRoutes = require('./routes/videos');
+const app = express();
+
+mongoose.connect(
+    'mongodb+srv://Preston:PrestonDb@cosmainmedia.6u5pq.mongodb.net/cosmainmedia?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    }
+    )
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch(() => {
+        console.log('Connection failed!');
+})
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Video Route
+app.use('/api/videos', videoRoutes)
+
+cron.schedule('25 10 * * *', () => {
+    deleteVideos('Trending');
+    fetchVideos();
+    deleteVideos('Interview');
+    updateCosmainVideos();
+})
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,44 +1,51 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cron = require('node-cron');
-require('dotenv').config();
-const {fetchVideos, updateCosmainVideos, getVideos, deleteVideos} = require('./controllers/videos');
+const app = require("./app");
+const http = require("http");
 
-// ROUTES
-const videoRoutes = require('./routes/videos');
-const app = express();
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-mongoose.connect(
-    'mongodb+srv://Preston:PrestonDb@cosmainmedia.6u5pq.mongodb.net/cosmainmedia?retryWrites=true&w=majority',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    }
-    )
-    .then(() => {
-        console.log('Connected to database!');
-    })
-    .catch(() => {
-        console.log('Connection failed!');
-})
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-// Video Route
-app.use('/api/videos', videoRoutes)
+  return false;
+};
 
-cron.schedule('25 10 * * *', () => {
-    deleteVideos('Trending');
-    fetchVideos();
-    deleteVideos('Interview');
-    updateCosmainVideos();
-})
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
-app.listen(3001, () => {
-    console.log('App is running on port 3001');
-});
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+};
+
+const port = normalizePort(process.env.PORT || "3001");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
